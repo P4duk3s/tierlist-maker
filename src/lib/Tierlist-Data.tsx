6cb27924afafd505e34ed2eb3717a27e5f94231f
P4duk3s <zygisbalcius@gmail.com>
2024-09-18 12:46:42 +0100

    tidy up

---
+++ src/lib/Tierlist-Data.tsx
@@ -0,0 +1,250 @@
+import { v4 } from "uuid"
+
+type IconData = {
+    id: string,
+    imgSrc: string,
+    group: "main" | (string & {})
+}
+
+export const carBrands: IconData[] = [
+    {
+        id: v4(),
+        imgSrc: "abarth.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "alfa-romeo.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "alpine.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "aston-martin.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "audi.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "bentley.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "bmw.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "bugatti.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "chevrolet.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "citroen.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "ferrari.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "fiat.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "ford.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "honda.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "jaguar.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "lamborghini.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "lancia.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "land-rover.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "lexus.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "maserati.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "mazda.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "mini.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "merc.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "nissan.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "opel.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "peugeot.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "porsche.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "renault.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "rolls-royce.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "rover.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "saab.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "seat.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "skoda.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "subaru.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "suzuki.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "tesla.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "toyota.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "vw.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "volvo.png",
+        group: "main"
+    }
+]
+
+export const f1Teams: IconData[] = [
+    {
+        id: v4(),
+        imgSrc: "f1-redbull.png",
+        group: "main"
+    },
+    {
+        id: v4(),
+        imgSrc: "f1-mclaren.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-ferrari.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-mercedes.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-aston-martin.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-racing-bulls.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-haas.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-williams.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-alpine.png",
+        group: "main"
+    }, {
+        id: v4(),
+        imgSrc: "f1-sauber.png",
+        group: "main"
+    }
+]
